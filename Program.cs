d0ec8ece9cab3734840fb700c339b3b1b38dfbd7
AdrianVeidt <13481513@mail.ru>
2016-01-03 21:40:03 +0600

    +
    Попробовал использовать моки. Пока не очень.

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using supervkapp;
 
-namespace supervkapp {
-	class Program {
-		static void Main(string[] args) {
+namespace supervkapp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			var vkobject = new Api();
-			var posts = vkobject.GetFriendsTopPosts("https://vk.com/egorveidt/", (7*24*3600), 10);
-			foreach (var post in posts) {
+			var posts = vkobject.GetFriendsTopPosts("https://vk.com/egorveidt/", (7 * 24 * 3600), 10);
+			foreach (var post in posts)
+			{
 				Console.WriteLine("PostID \t {0} \t ownerID: {1} \t likes: {2}", post.Id, post.OwnerId, post.LikesInfo.Count);
 			}
 			Console.ReadKey();
--- Tests.cs
+++ Tests.cs
@@ -5,22 +5,196 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using supervkapp;
+using Moq;
 
-namespace Tests {
-	class Test {
+
+namespace Tests
+{
+	class Test
+	{
 		[Test]
-		public void ParseUrlIsCorrect() {
+		public void ParseUrlIsCorrect()
+		{
 			Assert.AreEqual(Api.ParseUrl(@"https://vk.com/id404/"), "id404");
 			Assert.AreEqual(Api.ParseUrl(@"https://vk.com/egorveidt/"), "egorveidt");
 		}
 
 		[Test]
-		public void ParseUrlIsNOTCorrect() {
+		public void ParseUrlIsNOTCorrect()
+		{
 			var ex = Assert.Throws<Exception>(() => Api.ParseUrl(@"https://vk.com/12345"));
 			Assert.That(ex.Message, Is.EqualTo("Url is not correct."));
 
 		}
 
+		private Api _ApiTestUnit;
+
+		[Test]
+		public void LogicUnitTest()
+		{
+			var ApiTestUnit = new Mock<Api>();
+
+			ApiTestUnit.Setup(api => api.GetUser(1)).Returns(
+				new User()
+				{
+					Id = 1,
+					FirstName = "Egor",
+					LastName = "Veidt"
+				});
+
+			ApiTestUnit.Setup(api => api.GetUsers(1, 2, 3)).Returns(
+				new List<User>()
+				{
+					new User()
+					{
+						Id = 1,
+						FirstName = "Egor",
+						LastName = "Veidt"
+					},
+					new User()
+					{
+						Id = 2,
+						FirstName = "NeEgor",
+						LastName = "NeVeidt"
+					},
+					new User()
+					{
+						Id = 3,
+						FirstName = "lol",
+						LastName = "olo"
+					}
+				});
+
+			ApiTestUnit.Setup(api => api.ResolvePageAddress("egorveidt")).Returns(
+				new ResolveResult()
+				{
+					Id = 1,
+					Type = ResolveType.User
+				});
+			ApiTestUnit.Setup(api => api.GetFriends(1)).Returns(
+				new List<User>()
+				{
+					new User()
+					{
+						Id = 2,
+						FirstName = "NeEgor",
+						LastName = "NeVeidt"
+					},
+					new User()
+					{
+						Id = 3,
+						FirstName = "lol",
+						LastName = "olo"
+					}
+				});
+
+			ApiTestUnit.Setup(api => api.GetWallPosts(1, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 1,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 15
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 2,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 6
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 3,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 7
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					}
+				});
+			ApiTestUnit.Setup(api => api.GetWallPosts(2, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 4,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 8
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 5,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 9
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 6,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 10
+						},
+						Date = 3 * 24 * 3600
+					}
+				});
+			ApiTestUnit.Setup(api => api.GetWallPosts(3, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 7,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 1
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 8,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 2
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 9,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 3
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					}
+				});
+
+			_ApiTestUnit = ApiTestUnit.Object;
+		}
+
+		}
 		//[Test]
 		//public void GetFriendsTopPostsIsCorrect() {
 		//	var vkobject = new Api();
@@ -31,4 +205,3 @@ namespace Tests {
 		//}
 
 	}
-}
