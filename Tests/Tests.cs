4644871710d4008505314fdd6018b1041390aba9
AdrianVeidt <13481513@mail.ru>
2016-01-05 17:58:55 +0600

    +

---
+++ Tests/Tests.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using supervkapp;
+using Moq;
+
+
+namespace Tests
+{
+	class Test
+	{
+		[Test]
+		public static void ParseUrlIsCorrect()
+		{
+			Assert.AreEqual(Api.ParseUrl(@"https://vk.com/id404/"), "id404");
+			Assert.AreEqual(Api.ParseUrl(@"https://vk.com/egorveidt/"), "egorveidt");
+		}
+
+		[Test]
+		public static void ParseUrlIsNOTCorrect()
+		{
+			var ex = Assert.Throws<Exception>(() => Api.ParseUrl(@"https://vk.com/12345"));
+			Assert.That(ex.Message, Is.EqualTo("Url is not correct."));
+
+		}
+		/*
+		private Api _ApiTestUnit;
+
+		[Test]
+		public void LogicUnitTest()
+		{
+			var ApiTestUnit = new Mock<Api>();
+
+			ApiTestUnit.Setup(api => api.GetUser(1)).Returns(
+				new User()
+				{
+					Id = 1,
+					FirstName = "Egor",
+					LastName = "Veidt"
+				});
+
+			ApiTestUnit.Setup(api => api.GetUsers(1, 2, 3)).Returns(
+				new List<User>()
+				{
+					new User()
+					{
+						Id = 1,
+						FirstName = "Egor",
+						LastName = "Veidt"
+					},
+					new User()
+					{
+						Id = 2,
+						FirstName = "NeEgor",
+						LastName = "NeVeidt"
+					},
+					new User()
+					{
+						Id = 3,
+						FirstName = "lol",
+						LastName = "olo"
+					}
+				});
+
+			ApiTestUnit.Setup(api => api.ResolvePageAddress("egorveidt")).Returns(
+				new ResolveResult()
+				{
+					Id = 1,
+					Type = ResolveType.User
+				});
+			ApiTestUnit.Setup(api => api.ResolvePageAddress("group")).Returns(
+				new ResolveResult()
+				{
+					Id = 10,
+					Type = ResolveType.Group
+				});
+
+			ApiTestUnit.Setup(api => api.GetFriends(1)).Returns(
+				new List<User>()
+				{
+					new User()
+					{
+						Id = 2,
+						FirstName = "NeEgor",
+						LastName = "NeVeidt"
+					},
+					new User()
+					{
+						Id = 3,
+						FirstName = "lol",
+						LastName = "olo"
+					}
+				});
+
+			ApiTestUnit.Setup(api => api.GetWallPosts(1, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 1,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 15
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 2,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 6
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 3,
+						OwnerId = 1,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 7
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					}
+				});
+			ApiTestUnit.Setup(api => api.GetWallPosts(2, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 4,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 8
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 5,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 9
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 6,
+						OwnerId = 2,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 10
+						},
+						Date = 3 * 24 * 3600			// этот пост не попадает в выборку за последнюю неделю
+					}
+				});
+			ApiTestUnit.Setup(api => api.GetWallPosts(3, 10, 0)).Returns(
+				new List<Post>()
+				{
+					new Post()
+					{
+						Id = 7,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 1
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 8,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 2
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					},
+					new Post()
+					{
+						Id = 9,
+						OwnerId = 3,
+						LikesInfo = new AboutLikes()
+						{
+							Count = 3
+						},
+						Date = 1000 + (7 * 24 * 3600)
+					}
+				});
+
+			_ApiTestUnit = ApiTestUnit.Object;
+		}
+		*/
+	}
+	//[Test]
+	//public void GetFriendsTopPostsIsCorrect() {
+	//	var vkobject = new Api();
+	//	var posts = vkobject.GetFriendsTopPosts("vk.com/egorveidt", 7 * 24 * 3600, 10);
+	//	Assert.AreEqual(posts.Count, 10);
+	//	Assert.AreEqual(posts[0].LikesInfo.Count, 51);
+	//	Assert.AreEqual(posts[9].LikesInfo.Count, 37);
+	//}
+
+}
---
+++ supervkapp/Api.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using supervkapp;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+
+namespace supervkapp 
+{
+	public class Api 
+	{
+		//https://oauth.vk.com/authorize?client_id=5091660&display=page&redirect_uri=http://vk.com/callback&scope=friends&response_type=token&v=5.42
+
+		private const string Url = "https://api.vk.com/method/{0}?{1}&v=5.37&access_token=fdbb5f01397530c5bddfe9e888acbbe185610a77fb17444aec3f9a4f6ae021d823171ac1dc6b46df6e3ba";
+		public  ResolveResult ResolvePageAddress(string pageAddress) 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"utils.resolveScreenName",
+						String.Format("screen_name={0}", pageAddress)));
+
+				var response = JsonConvert.DeserializeObject<Reply<ResolveResult>>(Response);
+				if (response != null)
+					return response.Response;
+
+				return null;
+			}
+		}
+		public static string ParseUrl(string url) 
+		{
+			var regex = new Regex(@"(https?://)?(www\.)?vk\.com/(?<name>[a-z][a-z0-9\.]+)\/?");
+			var matches = regex.Matches(url.ToLower());
+			if (matches.Count != 0)
+				return matches[0].Groups["name"].Value;
+
+			throw new Exception("Url is not correct.");
+		}
+		public  User GetUser(uint id) 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"users.get",
+						String.Format("user_ids={0}", id)));
+
+				var response = JsonConvert.DeserializeObject<Reply<List<User>>>(Response);
+				if (response != null &&
+					response.Response != null &&
+					response.Response.Count != 0)
+					return response.Response[0];
+
+				return null;
+			}
+		}
+		public  List<User> GetUsers(params uint[] ids) 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"users.get",
+						String.Format("user_ids={0}", string.Join(",", ids))));
+
+				var response = JsonConvert.DeserializeObject<Reply<List<User>>>(Response);
+				if (response != null &&
+					response.Response != null)
+					return response.Response;
+
+				return new List<User>();
+			}
+		}
+		public  List<User> GetFriends(uint id) 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"friends.get",
+						String.Format("user_id={0}", id)));
+
+				var response = JsonConvert.DeserializeObject<Reply<FriendsIdsResponse>>(Response);
+				if (response != null &&
+					response.Response.Count != 0) 
+				{
+					var users = new List<User>((int)response.Response.Count);
+					users.AddRange(response.Response.FriendsIds.Select(userId => new User() 
+					{
+						Id = userId
+					}));
+
+					return users;
+				}
+
+				return new List<User>();
+			}
+		}
+		public  List<Post> GetWallPosts(int id, uint count, uint offset) 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"wall.get",
+						String.Format("owner_id={0}&Count={1}&offset={2}", id, count, offset)));
+
+				var response = JsonConvert.DeserializeObject<Reply<PostsResponse>>(Response);
+				if (response != null &&
+					response.Response != null)
+					return response.Response.Posts;
+
+				return new List<Post>();
+			}
+		}
+		public  List<Post> GetWallPosts(int id, uint dateMin) 
+		{
+			var posts = new List<Post>();
+			var prevPostsCount = 0;
+
+			const uint count = 10;
+			uint offset = 0;
+
+			do 
+			{
+				prevPostsCount = posts.Count;
+				var newPosts = GetWallPosts(id, count, offset);
+				posts.AddRange(newPosts.Where(post => post.Date >= dateMin));
+
+				offset += count;
+			} while ((posts.Count - prevPostsCount) == count);
+
+			return posts;
+		}
+		public List<Post> GetFriendsTopPosts(string userUrl, uint lastPeriod, int count) 
+		{
+			var name = ParseUrl(userUrl);
+			var resolveResult = this.ResolvePageAddress(name);
+
+			if (resolveResult.Type != ResolveType.User) 
+			{
+				throw new Exception("not a User");
+			}
+
+			var user = this.GetUser(resolveResult.Id);
+
+			var friends = this.GetFriends(user.Id);
+			friends.Add(user);
+
+			uint currentTime = this.GetServerTime();
+			var someTimeAgo = currentTime - lastPeriod;
+
+			var posts = new List<Post>();
+			foreach (var friend in friends)
+				posts.AddRange(this.GetWallPosts((int)friend.Id, someTimeAgo));
+
+			posts = posts.OrderByDescending(p => p.LikesInfo.Count).Take(count).ToList();
+			return posts;
+		}
+		public  uint GetServerTime() 
+		{
+			using (var webclient = new WebClient()) 
+			{
+				var Response = webclient.DownloadString(
+					String.Format(
+						Url,
+						"utils.getServerTime",
+						""));
+
+				var response = JsonConvert.DeserializeObject<Reply<uint>>(Response);
+				if (response != null)
+					return response.Response;
+
+				return 0;
+			}
+		}
+	}
+}
